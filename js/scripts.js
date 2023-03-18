207da525f7e189b61b246eff8986ae570cadfe0f
Silvio Di Santi <silvio.disanti@studio.unibo.it>
2023-03-18 17:03:54 +0100

    Added countdown and some update on texts

--- js/scripts.js
+++ js/scripts.js
@@ -239,6 +239,38 @@ $(document).ready(function () {
 
 /********************** Extras **********************/
 
+// Countdown
+(function () {
+    var countDownDate = new Date("Jun 10, 2023 10:30:00").getTime();
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+        // Get today's date and time
+        var now = new Date().getTime();
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+    
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Output the result in the relative element
+        document.getElementById("days").innerText = days,
+        document.getElementById("hours").innerText = hours,
+        document.getElementById("minutes").innerText = minutes,
+        document.getElementById("seconds").innerText = seconds;
+
+        //do something later when date is reached
+        if (distance < 0) {
+            document.getElementById("announcement").innerText = "JUST MARRIED!";
+            document.getElementById("countdown").style.display = "none";
+            document.getElementById("date").style.display = "none";
+            clearInterval(x);
+        }
+    }, 1000);
+}());
+
 // Google map
 function initMap() {
     var location = {lat: 35, lng: 88};
