afe4cc9689e95a4164fd2bd4bdd25f9776c517d4
Silvio Di Santi <silvio.disanti@studio.unibo.it>
2023-03-23 20:33:06 +0100

    Revert "update on add to calendar function"
    This reverts commit 254f4b86c5d8a9186d86337a97cad775cc4a4913.

--- js/scripts.js
+++ js/scripts.js
@@ -188,23 +188,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Pina & Max",
+            title: "Ram and Antara's Wedding",
 
             // Event start date
-            start: new Date('Jun 10, 2023 10:30'),
+            start: new Date('Nov 27, 2017 10:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            // end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Nov 29, 2017 00:00'),
 
             // Event Address
-            address: 'Villa Florio Ricevimenti, San Nicandro Garganico',
+            address: 'ITC Fortune Park Hotel, Kolkata',
 
             // Event Description
-            // description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
         }
     });
 
